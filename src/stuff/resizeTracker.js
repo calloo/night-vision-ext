11d2e59e7c929a1b4832403175316dcd67624fca
calloo <michael.ma.calloo@gmail.com>
2025-08-13 14:56:08 +0800

    cleanup resize tracker on chart destroy

--- src/stuff/resizeTracker.js
+++ src/stuff/resizeTracker.js
@@ -2,11 +2,12 @@
 // the chart props
 
 export default function resizeTracker(chart) {
+  const resizeObserver = new ResizeObserver((entries) => {
+    let rect = chart.root.getBoundingClientRect()
+    chart.width = rect.width
+    chart.height = rect.height
+  })
+  resizeObserver.observe(chart.root)
 
-    const resizeObserver = new ResizeObserver((entries) => {
-        let rect = chart.root.getBoundingClientRect()
-        chart.width = rect.width
-        chart.height = rect.height
-    })
-    resizeObserver.observe(chart.root)
+  return () => resizeObserver.disconnect()
 }
